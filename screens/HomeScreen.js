2f082ea35211e798cb7b8cc941a7578c75c36895
Zeida <Zeidaromen97@gmail.com>
2021-12-18 16:35:57 +0000

    folder hierarchy

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text
+                onPress={() => alert('This iS THE HOME SCREEN.')}
+                style={{ fontSize: 26, fontWeight: 'bold' }}> Home Screen</Text>
+        </View>
+    );
+}
