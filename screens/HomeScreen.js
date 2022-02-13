b6f4c07f1e40c7dda0aa00a6599989e9cbc397a8
Zeida <Zeidaromen97@gmail.com>
2022-02-13 13:00:20 +0000

    menu 3x3 without optimization

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,88 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import ChallengeList from './ChallengesListScreen';
+import { Pressable, ScrollView, StyleSheet, Text, View, Dimensions} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 export default function HomeScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#BEDEFF' }}>
-                <ChallengeList/>
-        </View>
-    );
+  return (
+      <View style={styles.container}>
+        <Text style={styles.text}>¿Qué área te gustaría mejorar?</Text>
+        <Pressable style={styles.button}>
+          <Ionicons name="ios-leaf-outline" color={'white'} size={30} />
+          <Text style={styles.buttonText}>{"Aire libre"}</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Ionicons name="md-barbell-outline" color={'white'} size={30} />
+          <Text style={styles.buttonText}>{"Deporte"}</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Ionicons name="md-cafe-outline" color={'white'} size={30} />
+          <Text style={styles.buttonText}>{"Ocio"}</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Ionicons name="ios-bed-outline" color={'white'} size={30} />
+          <Text style={styles.buttonText}>{"Descanso"}</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Ionicons name="md-fitness-outline" color={'white'} size={30} />
+          <Text style={styles.buttonText}>{"Salud"}</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Ionicons name="lock-closed" color={'white'} size={30} />
+          <Text style={styles.buttonText}>{""}</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Ionicons name="lock-closed" color={'white'} size={30} />
+          <Text style={styles.buttonText}>{""}</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Ionicons name="lock-closed" color={'white'} size={30} />
+          <Text style={styles.buttonText}>{""}</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Ionicons name="lock-closed" color={'white'} size={30} />
+          <Text style={styles.buttonText}>{""}</Text>
+        </Pressable>
+      </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#BEDEFF',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingVertical: 100,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    elevation: 3,
+    backgroundColor: '#FFF592',
+    borderWidth: 2,
+    height: 100,
+    width: 100,
+    margin:2,
+  },
+  buttonText: {
+    fontSize: 14,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
+    marginBottom: 5,
+  },
+  text: {
+    fontSize: 22,
+    lineHeight: 30,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
+    marginBottom: 10,
+  },
+});
