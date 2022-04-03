7f9c40dcac989bd923d38dbb4c64280ecd3c9331
Zeida <zeidaromen97@gmail.com>
2022-04-03 09:55:00 +0100

    reformating

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,14 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import Constants from 'expo-constants';
+import firebase from "firebase/app";
+import "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDxyk4AVdQGessn5M52XDG0E_gjXG52fPM",
-    authDomain: "mindcaretfg.firebaseapp.com",
-    projectId: "mindcaretfg",
-    storageBucket: "mindcaretfg.appspot.com",
-    messagingSenderId: "578300871737",
-    appId: "1:578300871737:web:ceb286f872583e8229f9e8"
+  apiKey: "AIzaSyDxyk4AVdQGessn5M52XDG0E_gjXG52fPM",
+  authDomain: "mindcaretfg.firebaseapp.com",
+  projectId: "mindcaretfg",
+  storageBucket: "mindcaretfg.appspot.com",
+  messagingSenderId: "578300871737",
+  appId: "1:578300871737:web:ceb286f872583e8229f9e8",
 };
 
 // Initialize Firebase
