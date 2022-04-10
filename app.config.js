932eb56080eddba888e3cd8ba208e605393c074d
Zeida <zeidaromen97@gmail.com>
2022-04-10 12:44:18 +0100

    config firebase

--- app.config.js
+++ app.config.js
@@ -1,40 +1,41 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
   expo: {
-    name: 'Mindcare',
-    slug: 'Mindcare',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './assets/icon.png',
+    name: "Mindcare",
+    slug: "Mindcare",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./images/logo.png",
     splash: {
-      image: './assets/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff'
+      image: "./images/logo.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
     updates: {
-      fallbackToCacheTimeout: 0
+      fallbackToCacheTimeout: 0,
     },
-    assetBundlePatterns: ['**/*'],
+    assetBundlePatterns: ["**/*"],
     ios: {
-      supportsTablet: true
+      supportsTablet: true,
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
-        backgroundColor: '#FFFFFF'
-      }
+        foregroundImage: "./images/logo.png",
+        backgroundColor: "#FFFFFF",
+      },
     },
     web: {
-      favicon: './assets/favicon.png'
+      favicon: "./assets/favicon.png",
     },
     extra: {
       apiKey: process.env.API_KEY,
       authDomain: process.env.AUTH_DOMAIN,
       projectId: process.env.PROJECT_ID,
+      databaseURL: DATABASE_URL,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID
-    }
-  }
+      appId: process.env.APP_ID,
+    },
+  },
 };
